1e8458d780588a18c34ddd878a76f209b4bd6b32
constROD <rialcantara21@gmail.com>
2023-04-04 05:25:12 +0800

    chore: delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# Zack and Zoey Learning Tools
-
-This is a collection of tools for learning and teaching kids.
--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Home from 'modules/Home';
-import { type NextPage } from 'next';
-
-const HomePage: NextPage = () => {
-  return <Home />;
-};
-
-export default HomePage;
